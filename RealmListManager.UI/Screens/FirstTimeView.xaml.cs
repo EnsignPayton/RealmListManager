c3dd52b8e09d6df0dd2f82d9c178f14676ef00b9
Logan Payton <Logan.Payton@javs.com>
2018-02-06 19:57:54 -0500

    First time screen

---
+++ RealmListManager.UI/Screens/FirstTimeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RealmListManager.UI.Screens
+{
+    /// <summary>
+    /// Interaction logic for FirstTimeView.xaml
+    /// </summary>
+    public partial class FirstTimeView : UserControl
+    {
+        public FirstTimeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RealmListManager.UI/Screens/FirstTimeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace RealmListManager.UI.Screens
+{
+    public class FirstTimeViewModel : Screen
+    {
+    }
+}
